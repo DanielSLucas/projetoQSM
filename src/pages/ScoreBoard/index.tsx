0a0915dee7929f8b35aa2480a9e7a71edd8a7b97
DanielSLucas <daniellucas-pms@hotmail.com>
2020-07-28 11:28:23 -0300

    ScoreBoard styles

--- src/pages/ScoreBoard/index.tsx
+++ src/pages/ScoreBoard/index.tsx
@@ -1,9 +1,36 @@
 import React from 'react';
 
-import { Container } from './styles';
+import {
+  Container,
+  ScreenTitle,
+  ScoreText,
+  ScoreDetails,
+  ScoreDetailsText,
+  CharacterScore,
+  Button,
+  ButtonText,
+} from './styles';
 
 const ScoreBoard: React.FC = () => {
-  return <Container />
+  return (
+    <Container>
+      <ScreenTitle>PONTOS</ScreenTitle>
+
+      <ScoreText>12</ScoreText>
+
+      <ScoreDetails>
+        <ScoreDetailsText>PERSONAGENS</ScoreDetailsText>
+
+        <CharacterScore>1 - Personagem 1 ....... 5</CharacterScore>
+        <CharacterScore>2 - Personagem 2 ....... 4</CharacterScore>
+        <CharacterScore>3 - Personagem 3 ....... 3</CharacterScore>
+      </ScoreDetails>
+
+      <Button>
+        <ButtonText>JOGAR NOVAMENTE</ButtonText>
+      </Button>
+    </Container>
+  );
 };
 
 export default ScoreBoard;
--- src/pages/ScoreBoard/styles.tsx
+++ src/pages/ScoreBoard/styles.tsx
@@ -1,3 +1,65 @@
 import styled from 'styled-components/native';
 
-export const Container = styled.View``;
+export const Container = styled.View`
+  flex: 1;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const ScreenTitle = styled.Text`
+  font-family: "RubikMonoOne-Regular";
+  font-size: 48px;
+  text-align: center;
+  color: #FFF;
+  margin-top: 0px;
+`;
+
+export const ScoreText = styled.Text`
+  font-family: "RubikMonoOne-Regular";
+  font-size: 124px;
+  text-align: center;
+  color: #FFF;
+  margin-top: 20px;
+`;
+
+export const ScoreDetails = styled.View`
+  margin: 30px;
+  width: 320px;
+`;
+
+export const ScoreDetailsText = styled.Text`
+  font-family: "RubikMonoOne-Regular";
+  font-size: 22px;
+  text-align: center;
+  color: #FFF;
+  margin-bottom: 10px;
+`;
+
+export const CharacterScore = styled.Text`
+  font-family: "Roboto-Bold";
+  font-size: 24px;
+  color: #FFF;
+  margin: 5px;
+  text-align: center;
+`;
+
+export const Button = styled.View`
+  width: 250px;
+  height: 64px;
+  margin: 10px;
+  margin-top: 80px;
+
+  background: #1ABC9C;
+  border: 1.5px solid #FFF;
+  border-radius: 16px;
+
+  justify-content: center;
+  align-items: center;
+`;
+
+export const ButtonText = styled.Text`
+  color: #FFF;
+  font-family: "RubikMonoOne-Regular";
+  font-size: 20px;
+  text-align: center;
+`;
